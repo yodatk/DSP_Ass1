755ad71bf3f35b05797feee1659b489000503b46
Tomer <yodatk@gmail.com>
2020-12-01 23:23:32 +0200

    fixed stuff in manager, local and worker

--- dsp211Ass1Manager/src/main/java/manager/HtmlParser.java
+++ dsp211Ass1Manager/src/main/java/manager/HtmlParser.java
@@ -14,22 +14,78 @@ import java.util.Map;
 
 public class HtmlParser {
 
-    public static final String HTML_HEADER= "<html>\n\t<head>\n\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\"><title>OCR</title>\n\t</head>\n\t<body>\n";
+    public static final String HTML_HEADER = "<html>\n\t<head>\n\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\"><title>OCR</title>\n\t</head>\n\t<body>\n";
     public static final String HTML_FOOTER = "\t</body>\n</html>";
-    public static final String HTML_START_PARAGRAPH_AND_IMAGE = "\t\t<p>\n" +"\t\t<img src=";
+    public static final String HTML_START_PARAGRAPH_AND_IMAGE = "\t\t<p>\n" + "\t\t<img src=";
     public static final String HTML_AFTER_IMAGE_BEFORE_TEXT = "><br>\n\t\t";
     public static final String HTML_AFTER_TEXT = "\n\t\t</p>\n";
 
 
-    public HtmlParser() {
+    private File htmlFile;
+    private String fileName;
+
+    public File getHtmlFile() {
+        return htmlFile;
     }
 
-    public void parseListOfUrlAndTextToHTML(Map<String,String> urlsToText, String fileName){
-        File f = new File(fileName+".html");
-        try{
+    public String getFileName() {
+        return fileName;
+    }
+
+    public HtmlParser(String fileName) {
+        this.fileName = fileName + ".html";
+        this.htmlFile = new File(this.fileName);
+
+    }
+
+    public boolean initFile() {
+        return this.writeConstantToFile(HTML_HEADER);
+    }
+
+    private boolean writeConstantToFile(String toWrite) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(this.htmlFile));
+            bw.write(toWrite);
+            bw.close();
+            return true;
+        } catch (IOException e) {
+            System.out.println("problem with io");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean endFile() {
+        return this.writeConstantToFile(HTML_FOOTER);
+    }
+
+    public boolean appendListOfUrlAndTextToHTML(Map<String, String> urlsToText) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(this.htmlFile));
+            for (Map.Entry<String, String> imageWithParse : urlsToText.entrySet()) {
+
+                String paragraph = HTML_START_PARAGRAPH_AND_IMAGE + imageWithParse.getKey() +
+                        HTML_AFTER_IMAGE_BEFORE_TEXT +
+                        imageWithParse.getValue() +
+                        HTML_AFTER_TEXT;
+                bw.write(paragraph);
+            }
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("problem with io");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+
+    public static void parseListOfUrlAndTextToHTML(Map<String, String> urlsToText, String fileName) {
+        File f = new File(fileName + ".html");
+        try {
             BufferedWriter bw = new BufferedWriter(new FileWriter(f));
             bw.write(HTML_HEADER);
-            for(Map.Entry<String,String> imageWithParse : urlsToText.entrySet()){
+            for (Map.Entry<String, String> imageWithParse : urlsToText.entrySet()) {
 
                 String paragraph = HTML_START_PARAGRAPH_AND_IMAGE + imageWithParse.getKey() +
                         HTML_AFTER_IMAGE_BEFORE_TEXT +
@@ -39,49 +95,17 @@ public class HtmlParser {
             }
             bw.write(HTML_FOOTER);
             bw.close();
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("problem with io");
             e.printStackTrace();
         }
     }
 
-    private void try1() {
-        HtmlView view = StaticHtml
-                .view()
-                .html()
-
-                    // region head
-                    .head()
-                        .meta().attrHttpEquiv(EnumHttpEquivType.CONTENT_TYPE).attrContent("text/html; charset=windows-1252").__()
-                        .title().text("OCR").__()
-                    .__() //head
-                    // endregion head
-
-                    // region body
-                    .body()
-
-                        .p()
-                            .img()
-                                .attrSrc("http://ct.mob0.com/Fonts/CharacterMap/ocraextended.png")
-
-                            .__()
-                            .br().__()
-                            .text("... Parsed Text ...")
-
-
-                        .__()
-                    .__()
-                    //endregion body
-                .__(); //html
-        String s = view.render();
-        System.out.println(s);
-    }
-
     public static void main(String[] args) {
-        HtmlParser parser = new HtmlParser();
-        Map<String,String> input = new HashMap<>();
-        input.put("\"http://ct.mob0.com/Fonts/CharacterMap/ocraextended.png\"","...TEXT THAT WAS PARSED...");
-        parser.parseListOfUrlAndTextToHTML(input,"output");
+        HtmlParser parser = new HtmlParser("temp");
+        Map<String, String> input = new HashMap<>();
+        input.put("\"http://ct.mob0.com/Fonts/CharacterMap/ocraextended.png\"", "...TEXT THAT WAS PARSED...");
+        HtmlParser.parseListOfUrlAndTextToHTML(input, "output");
     }
 
 
--- dsp211Ass1Manager/src/main/java/manager/ManagerMain.java
+++ dsp211Ass1Manager/src/main/java/manager/ManagerMain.java
@@ -1,32 +1,12 @@
 package manager;
 
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 
 public class ManagerMain {
 
 
     public static void main(String[] args) {
-
-
-        HtmlParser htmlParser = new HtmlParser();
-
-        Map<String, String> localsQueues = new HashMap<>();
-
-        if (args.length < 2) {
-            System.out.println("AWS key and 'n' must be inserted");
-            return;
-        }
-
-
-        String awsKey = args[0];
-        String imagesLocationInS3 = args[1];
-        int n = Integer.parseInt(args[1]);
-
-        Manager manager = new Manager(n, awsKey);
+        Manager manager = new Manager();
         manager.run();
 
     }

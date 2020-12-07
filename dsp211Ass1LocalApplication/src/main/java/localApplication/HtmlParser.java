494adf54a159a35c31d31959ddcff6c16bc70da3
Tomer <yodatk@gmail.com>
2020-12-07 17:45:22 +0200

    html now working fine

--- dsp211Ass1LocalApplication/src/main/java/localApplication/HtmlParser.java
+++ dsp211Ass1LocalApplication/src/main/java/localApplication/HtmlParser.java
@@ -1,17 +1,32 @@
 package localApplication;
 
+import javafx.util.Pair;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HtmlParser {
 
+    /**
+     * Html header to to write in the start of the output html file
+     */
     public static final String HTML_HEADER = "<html>\n\t<head>\n\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\"><title>OCR</title>\n\t</head>\n\t<body>\n";
+    /**
+     * Html footer to to write in the end of the output html file
+     */
     public static final String HTML_FOOTER = "\t</body>\n</html>";
+    /**
+     * HTML tags to start paragraph and image in out put file
+     */
     public static final String HTML_START_PARAGRAPH_AND_IMAGE = "\t\t<p>\n" + "\t\t<img src=";
+    /**
+     * HTML tags to end image and start parsed text in ht
+     */
     public static final String HTML_AFTER_IMAGE_BEFORE_TEXT = "><br>\n\t\t";
     public static final String HTML_AFTER_TEXT = "\n\t\t</p>\n";
 
@@ -39,7 +54,7 @@ public class HtmlParser {
 
     private boolean writeConstantToFile(String toWrite) {
         try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(this.htmlFile,true));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(this.htmlFile, true));
             bw.write(toWrite);
             bw.close();
             return true;
@@ -54,10 +69,10 @@ public class HtmlParser {
         return this.writeConstantToFile(HTML_FOOTER);
     }
 
-    public boolean appendListOfUrlAndTextToHTML(Map<String, String> urlsToText) {
+    public boolean appendListOfUrlAndTextToHTML(List<Pair<String, String>> urlsToText) {
         try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(this.htmlFile,true));
-            for (Map.Entry<String, String> imageWithParse : urlsToText.entrySet()) {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(this.htmlFile, true));
+            for (Pair<String, String> imageWithParse : urlsToText) {
 
                 String paragraph = HTML_START_PARAGRAPH_AND_IMAGE + imageWithParse.getKey() +
                         HTML_AFTER_IMAGE_BEFORE_TEXT +
@@ -78,7 +93,7 @@ public class HtmlParser {
     public static void parseListOfUrlAndTextToHTML(Map<String, String> urlsToText, String fileName) {
         File f = new File(fileName + ".html");
         try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(f,true));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(f, true));
             bw.write(HTML_HEADER);
             for (Map.Entry<String, String> imageWithParse : urlsToText.entrySet()) {
 
